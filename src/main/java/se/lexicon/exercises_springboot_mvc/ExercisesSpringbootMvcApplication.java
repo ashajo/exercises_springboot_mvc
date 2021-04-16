187fded73ad1fc5e42498a74f0d95850d0681092
asha joseph <ashajo183@gmail.com>
2021-04-16 19:55:34 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/exercises_springboot_mvc/ExercisesSpringbootMvcApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.exercises_springboot_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercisesSpringbootMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExercisesSpringbootMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/exercises_springboot_mvc/ExercisesSpringbootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.exercises_springboot_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercisesSpringbootMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
